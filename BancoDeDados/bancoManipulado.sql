b703dc25581d87031da3e0a82038940f09341dcb
VG083 <83486329+VG083@users.noreply.github.com>
2022-10-18 09:18:09 -0300

    Update bancoManipulado.sql

--- BancoDeDados/bancoManipulado.sql
+++ BancoDeDados/bancoManipulado.sql
@@ -14,3 +14,15 @@ SELECT * FROM tb_cliente;
 
 SELECT telefone FROM tb_cliente
 WHERE nome='Ana Maria';
+SELECT cpf, telefone FROM tb_cliente WHERE tipo='VIP';
+SELECT cpf, telefone FROM tb_cliente 
+WHERE tipo='VIP' AND codigo=124;
+
+SELECT * FROM tb_produto;
+SELECT * FROM tb_produto WHERE preco>100;
+SELECT * FROM tb_produto WHERE preco<100;
+SELECT * FROM tb_produto WHERE preco>=295.50;
+SELECT * FROM tb_produto WHERE preco>295.50;
+SELECT * FROM tb_produto WHERE preco>=200 AND preco<=1000;
+SELECT * FROM tb_produto WHERE preco IS NULL;
+SELECT * FROM tb_produto WHERE preco IS NOT NULL;
